2ec72402585c9896b4310a2a64e716ae643192e9
knndpaz35 <kenneth.depaz35@gmail.com>
2025-10-13 19:05:01 +0800

    new ui ser

--- fractions/src/App.js
+++ fractions/src/App.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import Homepage from './screens/Homepage';
-import Reports from './screens/Reports';
-import DetailedReport from './screens/DetailedReport';
-import StuedentReport from './screens/StuedentReport';
+import React, { useState } from "react";
+import "./App.css";
+import Homepage from "./screens/Homepage";
+import Reports from "./screens/Reports";
+import DetailedReport from "./screens/DetailedReport";
+import StuedentReport from "./screens/StuedentReport";
 
 function App() {
-  const [page, setPage] = useState('home');
+  const [page, setPage] = useState("home");
   const [selectedSection, setSelectedSection] = useState(null);
   const [selectedStudent, setSelectedStudent] = useState(null);
 
   // Navigation handler for stack navigation
   const handleNavigate = (nextPage, data) => {
-    if (nextPage === 'detailedreport') {
+    if (nextPage === "detailedreport") {
       setSelectedSection(data);
-      setPage('detailedreport');
-    } else if (nextPage === 'studentreport') {
+      setPage("detailedreport");
+    } else if (nextPage === "studentreport") {
       setSelectedStudent(data);
-      setPage('studentreport');
+      setPage("studentreport");
     } else {
       setPage(nextPage);
     }
@@ -25,25 +25,13 @@ function App() {
 
   return (
     <div className="App">
-      {page === 'home' && (
-        <Homepage onNavigate={handleNavigate} />
+      {page === "home" && <Homepage onNavigate={handleNavigate} />}
+      {page === "reports" && <Reports onNavigate={handleNavigate} />}
+      {page === "detailedreport" && (
+        <DetailedReport section={selectedSection} onNavigate={handleNavigate} />
       )}
-      {page === 'reports' && (
-        <Reports
-          onNavigate={handleNavigate}
-        />
-      )}
-      {page === 'detailedreport' && (
-        <DetailedReport
-          section={selectedSection}
-          onNavigate={handleNavigate}
-        />
-      )}
-      {page === 'studentreport' && (
-        <StuedentReport
-          student={selectedStudent}
-          onNavigate={handleNavigate}
-        />
+      {page === "studentreport" && (
+        <StuedentReport student={selectedStudent} onNavigate={handleNavigate} />
       )}
     </div>
   );
